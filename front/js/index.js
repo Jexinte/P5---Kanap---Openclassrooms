58891289511655e505abe31d0fd557b9be28ccbb
Mamadou.D.D <mdembelepro@gmail.com>
2022-02-17 14:37:39 +0100

    Implementation of color in select option is done

--- front/js/index.js
+++ front/js/index.js
@@ -1,8 +1,8 @@
 // API REQUEST
 const connection = fetch ('http://localhost:3000/api/products')
 
-/* *********CONNEXION*********** */
-connection.then((response) => {
+/* *********CONNECTION*********** */
+.then((response) => {
     
     if(response.ok){
         return response.json();
@@ -14,59 +14,65 @@ connection.then((response) => {
     
 })
 
-/* ********FIN DE CONNEXION*********** */
+/* ********END OF CONNECTION*********** */
 
 .then((jsonArray)=>{
 
-    
+    // Remove the first link replaced later
     const firstLink = document.querySelector("#items > a");
     firstLink.remove();
     
     
    
-        
-    function ourData(data_json){
+    // Built for create new element
+    function ourData(data_json)
+    {
 
-      
-        for(let i = 0; i < data_json.length; i++)
-        {
-            
-            // Targeting
-            const targetingItems = document.getElementById('items');
-            
-            // Creating Elements
-            
-            const createImgProduct = document.createElement('img');
-            const createTitleProduct = document.createElement('h3');
-            const createParagraphProduct = document.createElement('p');
-            const creatingALink = document.createElement('a');
-            const createArticle = document.createElement('article');
+        // Loop to get all json elements
+            for(let i = 0; i < data_json.length; i++)
+            {
+                
+                // Targeting
+                const targetingItems = document.getElementById('items');
+                
+                // Creating Elements
+                
+                const createImgProduct = document.createElement('img');
+                const createTitleProduct = document.createElement('h3');
+                const createParagraphProduct = document.createElement('p');
+                const creatingALink = document.createElement('a');
+                const createArticle = document.createElement('article');
 
-            
-            
-            // Nesting elements
-            targetingItems.appendChild(creatingALink);
-            creatingALink.appendChild(createArticle);
-            createArticle.appendChild(createImgProduct);
-            createArticle.appendChild(createTitleProduct);
-            createArticle.appendChild(createParagraphProduct);
+                
+                
+                // Nesting elements
+                targetingItems.appendChild(creatingALink);
+                creatingALink.appendChild(createArticle);
+                createArticle.appendChild(createImgProduct);
+                createArticle.appendChild(createTitleProduct);
+                createArticle.appendChild(createParagraphProduct);
 
-            // Giving a classname to some elements created before
-            createTitleProduct.className = "productName";
-            createParagraphProduct.className = "productDescription";
+                // Giving a classname to some elements created before
+                createTitleProduct.className = "productName";
+                createParagraphProduct.className = "productDescription";
 
-            // Insert elements from api
+                // Insert of all products on homepage
+                
+                createImgProduct.src = `${data_json[i].imageUrl}`;
+                createImgProduct.alt = `${data_json[i].altTxt}`;
+                createTitleProduct.innerHTML = `${data_json[i].name}`;
+                createParagraphProduct.innerHTML = `${data_json[i].description}`;
+
+                // This is made to recover a single product for each link on product page
+                creatingALink.href = "./product.html?id="+`${data_json[i]._id}`+"&"+"name="+
+                `${data_json[i].name}`+"&"+
+                "price="+`${data_json[i].price}`+"&"+"description="+
+                `${data_json[i].description}`+"&"+
+                "img="+`${data_json[i].imageUrl}`+"&"+
+                "colors="+`${data_json[i].colors}`;
+                
             
-            createImgProduct.src = `${data_json[i].imageUrl}`;
-            createImgProduct.alt = `${data_json[i].altTxt}`;
-            createTitleProduct.innerHTML = `${data_json[i].name}`;
-            createParagraphProduct.innerHTML = `${data_json[i].description}`;
-            creatingALink.href = "./product.html?id="+`${data_json[i]._id}`+"&"+"name="+
-            `${data_json[i].name}`+"&"+
-            "price="+`${data_json[i].price}`+"&"+"description="+
-            `${data_json[i].description}`+"&"+
-            "img="+`${data_json[i].imageUrl}`+"&"+"colors="+`${data_json[i].colors}`;
-        }
+            }
 
     }
 
--- front/js/product.js
+++ front/js/product.js
@@ -2,12 +2,12 @@
 const connection = fetch ('http://localhost:3000/api/products')
 
 /* *********CONNECTION*********** */
-connection.then((response) => {
+.then((response) => {
     
     if(response.ok){
         return response.json();
     }
-
+    
     else{
         console.log('Connection failed');
     }
@@ -17,35 +17,80 @@ connection.then((response) => {
 /* ********END OF CONNECTION*********** */
 
 
-    
     const search_params = new URLSearchParams(window.location.search);
     
-
+    
     // In case of the paramaters exists 
-    if(search_params.has('name') && search_params.has('price') && search_params.has('description') && search_params.has('img')){
+    if(
+    search_params.has('name') && 
+    search_params.has('price')&& 
+    search_params.has('description')&&
+    search_params.has('img') && search_params.has('colors'))
+    {
 
         // This allow to recover the value of each paramaters
         const name = search_params.get('name');
         const price = search_params.get('price');
         const description = search_params.get('description');
         const img = search_params.get('img');
+
+        
+        // Targeting Items
         const titleOfTheActualProduct = document.querySelector(".item__content__titlePrice > #title");
         const priceOfTheActualProduct = document.querySelector(" .item__content__titlePrice #price");
         const descriptionOfTheActualProduct = document.querySelector('.item__content__description #description');
         const imgOfTheActualProduct = document.querySelector(".item__img > img");
-
-        // Insert of parameters value
+        
+    
+        // Insert of parameters value in all targets selectors
         titleOfTheActualProduct.innerHTML=name;
         priceOfTheActualProduct.innerHTML =price;
         descriptionOfTheActualProduct.innerHTML = description;
         imgOfTheActualProduct.src=img;
-    
     }
 
-    else{
+    else
+    {
         console.log('Data recovery failed, please check the configuration of the condition');
     }
 
+     // Targeting Items
+    const colorOfTheActualProduct = document.querySelector(".item__content__settings__color #colors");
 
+    // Recover the parameter values in the query string
+    const color_options = search_params.get('colors');
+    // Split the parameter values in unique values
+    let numberOfColors = color_options.split(',');
+    
+    // Insert the right color according to the number of values present on the array
+    switch(numberOfColors.length)
+    {
 
-
+        case 2:
+            colorOfTheActualProduct.children;
+                      colorOfTheActualProduct[0].innerHTML=numberOfColors[0];
+                      colorOfTheActualProduct[1].innerHTML=numberOfColors[1];
+                    //   Deleted the element unuseful
+                      colorOfTheActualProduct[2].remove();
+                      break;
+        case 3:
+            
+           colorOfTheActualProduct.children;
+                       colorOfTheActualProduct[0].innerHTML=numberOfColors[0];
+                       colorOfTheActualProduct[1].innerHTML=numberOfColors[1];
+                       colorOfTheActualProduct[2].innerHTML=numberOfColors[2];
+                    break;
+        case 4:
+       
+                      const newOption = document.createElement('option');
+                      colorOfTheActualProduct.appendChild(newOption)
+                      colorOfTheActualProduct.children;
+                      colorOfTheActualProduct[0].innerHTML=numberOfColors[0];
+                      colorOfTheActualProduct[1].innerHTML=numberOfColors[1];
+                      colorOfTheActualProduct[2].innerHTML=numberOfColors[2];
+                      colorOfTheActualProduct[3].innerHTML=numberOfColors[3];
+                      break;
+                      
+        default:
+             console.log('Data recovery failed, please check the configuration of the condition');
+      }
