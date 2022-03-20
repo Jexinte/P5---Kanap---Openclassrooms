f47b330660b52d2d14f6a13806dd7242a902eb90
Mamadou.D.D <mdembelepro@gmail.com>
2022-03-20 09:46:20 +0100

    Refactoring

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,7 @@
-const displayNumber = document.getElementById('orderId')
+const displayOrderId = document.getElementById('orderId')
 const search_params = new URLSearchParams(window.location.search)
 
 if(search_params.has('orderId')){
-    displayNumber.innerHTML = search_params.get('orderId')
+    displayOrderId.innerHTML = search_params.get('orderId')
     localStorage.clear()
 }
